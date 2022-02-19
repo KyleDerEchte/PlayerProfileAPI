e842cba12ea13bf5dde8dba4bdb8f1b6d1ee0272
kyleonaut <kyleonaut@gmail.com>
2022-02-19 16:09:10 +0100

    Initial Commit

---
+++ src/main/java/de/kyleonaut/playerprofile/PlayerProfilePlugin.java
@@ -0,0 +1,16 @@
+package de.kyleonaut.playerprofile;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author kyleonaut
+ * @version 1.0.0
+ * created at 19.02.2022
+ */
+public class PlayerProfilePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
